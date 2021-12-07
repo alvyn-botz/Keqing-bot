92a67eca88f141584f9496f56033d7c6db0c4f2d
anaskautsar7 <94693802+anaskautsar7@users.noreply.github.com>
2021-12-07 20:55:44 +0700

    Add files via upload

---
+++ shop/allpayment.js
@@ -0,0 +1,21 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY VYNUTZXX22
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0887 4350 47326
+2. OVO
+	ㅁ 0887 4350 47326
+3. DANA
+	ㅁ 0887 4350 47326
+
+*📲 PULSA*
+    ㅁ 0859193578139
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
